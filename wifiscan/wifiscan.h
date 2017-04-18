6bf21d98d66fa457b7864ac263745e4e717a6081
JIANGYIFENGPI <472231536@qq.com>
2017-04-18 09:06:08 +0300

    Update wifiscan.h

--- wifiscan/wifiscan.h
+++ wifiscan/wifiscan.h
@@ -7,12 +7,16 @@ typedef struct{
 }WIFI_info;
 
 #define CMD "sudo iwlist wlan0 scan | \"Address|Frequency|ESSID|Quality\" > scan.txt"
+#define SFILE "scan.txt"
 
-//function prototypes
+// function prototypes
 void read_data(void);
-void removepaces(char []);
-void get_MAC(char [],char []);
-double get_frequency(char []);
-int get_channel(char []);
-double get_slevel(char []);
-void get_essid(char [], char []);
+void remove_spaces(char []);	// this function removes leading spaces of a string/line
+void get_MAC(char [], char []);	// this function reads MAC address of AP from 1st line
+double get_frequency(char []);	// this function reads carrier frequency from 2nd line
+int get_channel(char []);		// this function read channel number from 2nd line
+int get_slevel(char []); 	// this function reads signal level from 3rd line
+void get_essid(char [], char []); 	// this function reads ESSID from the 4th line
+void displayNetworks(WIFI_INFO [], int);	// display networks on screen
+void disp(WIFI_INFO, int); // display one network with index number (color code)
+
